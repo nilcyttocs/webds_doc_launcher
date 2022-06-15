8da5cf70fcb36a32a820ec914776d4eed9fa5a09
Scott Lin <scott.lin@tw.synaptics.com>
2022-06-14 23:44:49 -0700

    Refactor code and switch to using WebDSWidget

--- src/index.ts
+++ src/index.ts
@@ -2,68 +2,78 @@ import {
   ILayoutRestorer,
   JupyterFrontEnd,
   JupyterFrontEndPlugin
-} from '@jupyterlab/application';
+} from "@jupyterlab/application";
 
-import {
-  MainAreaWidget,
-  WidgetTracker
-} from '@jupyterlab/apputils';
+import { WidgetTracker } from "@jupyterlab/apputils";
 
-import { ILauncher } from '@jupyterlab/launcher';
+import { ILauncher } from "@jupyterlab/launcher";
 
-import { Widget } from '@lumino/widgets';
+import { Widget } from "@lumino/widgets";
 
-import { WebDSDocLauncher } from './launcher';
+import { WebDSWidget } from "@webds/service";
 
-import { 
-  documentationIcon,
-  webdsIcon
-} from './icons';
+import { WebDSDocLauncher } from "./launcher";
+
+import { documentationIcon, webdsIcon } from "./icons";
 
 /**
  * Initialization data for the @webds/doc_launcher extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: '@webds/doc_launcher:plugin',
+  id: "@webds/doc_launcher:plugin",
   autoStart: true,
   requires: [ILauncher, ILayoutRestorer],
-  activate: (app: JupyterFrontEnd, launcher: ILauncher, restorer: ILayoutRestorer) => {
-    console.log('JupyterLab extension @webds/doc_launcher is activated!');
+  activate: (
+    app: JupyterFrontEnd,
+    launcher: ILauncher,
+    restorer: ILayoutRestorer
+  ) => {
+    console.log("JupyterLab extension @webds/doc_launcher is activated!");
 
-    let widget: MainAreaWidget;
+    let widget: WebDSWidget;
     const { commands, shell } = app;
-    const command: string = 'webds_doc_launcher:open';
+    const command: string = "webds_doc_launcher:open";
     commands.addCommand(command, {
-      label: 'Documentation',
-      caption: 'Documentation',
-      icon: (args: { [x: string]: any }) => (args['isLauncher'] ? documentationIcon : undefined),
+      label: "Documentation",
+      caption: "Documentation",
+      icon: (args: { [x: string]: any }) =>
+        args["isLauncher"] ? documentationIcon : undefined,
       execute: () => {
         if (!widget || widget.isDisposed) {
           const callback = (item: Widget): void => {
-            shell.add(item, 'main');
+            shell.add(item, "main");
           };
-          const docLauncher = new WebDSDocLauncher(commands, launcher.items(), callback);
-          widget = new MainAreaWidget({ content: docLauncher });
-          widget.id = 'webds_doc_launcher_widget';
-          widget.title.label = 'WebDS - Documentation';
+          const docLauncher = new WebDSDocLauncher(
+            commands,
+            launcher.items(),
+            callback
+          );
+          widget = new WebDSWidget({ content: docLauncher });
+          widget.id = "webds_doc_launcher_widget";
+          widget.title.label = "WebDS - Documentation";
           widget.title.icon = webdsIcon;
           widget.title.closable = true;
         }
 
-        if (!tracker.has(widget))
-          tracker.add(widget);
+        if (!tracker.has(widget)) tracker.add(widget);
 
-        if (!widget.isAttached)
-          shell.add(widget, 'main');
+        if (!widget.isAttached) shell.add(widget, "main");
 
         shell.activateById(widget.id);
-      },
+      }
     });
 
-    launcher.add({ command, args: { isLauncher: true }, category: 'WebDS', rank: 1 });
+    launcher.add({
+      command,
+      args: { isLauncher: true },
+      category: "WebDS",
+      rank: 1
+    });
 
-    let tracker = new WidgetTracker<MainAreaWidget>({ namespace: 'webds_doc_launcher' });
-    restorer.restore(tracker, { command, name: () => 'webds_doc_launcher' });
+    let tracker = new WidgetTracker<WebDSWidget>({
+      namespace: "webds_doc_launcher"
+    });
+    restorer.restore(tracker, { command, name: () => "webds_doc_launcher" });
   }
 };
 
