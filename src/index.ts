1a3a63cf1bebe9d98c1a8cd34ba8a5fd900c219e
Scott Lin <scott.lin@tw.synaptics.com>
2022-12-06 10:33:39 +0800

    Remove await for WebDS Service

--- src/index.ts
+++ src/index.ts
@@ -41,10 +41,6 @@ const plugin: JupyterFrontEndPlugin<void> = {
   ) => {
     console.log("JupyterLab extension @webds/doc_launcher is activated!");
 
-    if (service) {
-      await service.initialized;
-    }
-
     let widget: WebDSWidget;
     const { commands, shell } = app;
     const command = Attributes.command;
